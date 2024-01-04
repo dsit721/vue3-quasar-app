58d03473d32307d34ac8ccf82855b1ac8c5ae0a6
StudyCow <dsit.kr@gmail.com>
2024-01-05 00:41:18 +0900

    섹션1-Auto Page&Layout 설정-Auto Page 설정 : unplugin-vue-router

---
+++ src/pages/components/TempComp.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="text-h4">TempComp</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="text-h4">Home</div>
+    <div>{{ $route.name }}</div>
+    <div>{{ $route.meta }}</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
+
+<route lang="yaml">
+name: home-page
+meta:
+  requiresAuth: true
+</route>
